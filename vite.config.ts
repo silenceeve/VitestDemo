8c626e57368f90a7dc72596686cfcb9fd7a319b3
Silenceeve Zeya <906517534@qq.com>
2024-07-19 14:14:51 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+    // do not use import.meta.vitest in your code when building
+    define: {
+        "import.meta.vitest": "undifined",
+    },
     test: {
         // in case you have test in function ts file
         includeSource: ["src/**/*.{ts,tsx,js,jsx}"],
